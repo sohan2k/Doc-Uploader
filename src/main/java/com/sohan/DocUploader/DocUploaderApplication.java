4ed60bbc5e67eb33ea05bf87212b0ebc2de848a2
Sohan Barman <barmansohan9@mail.com>
2022-07-14 18:34:13 +0530

    initial

---
+++ src/main/java/com/sohan/DocUploader/DocUploaderApplication.java
@@ -0,0 +1,13 @@
+package com.sohan.DocUploader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocUploaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocUploaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sohan/DocUploader/DocUploaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sohan.DocUploader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocUploaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
