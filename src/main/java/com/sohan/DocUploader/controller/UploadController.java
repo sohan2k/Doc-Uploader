c1cdb2d7f17256b7c81dd5453466198ec065db05
Sohan Barman <barmansohan9@mail.com>
2022-07-14 23:17:09 +0530

    image upload controller

---
+++ src/main/java/com/sohan/DocUploader/controller/UploadController.java
@@ -0,0 +1,38 @@
+package com.sohan.DocUploader.controller;
+
+import com.sohan.DocUploader.SingleTextUploadService;
+import com.sohan.DocUploader.service.ImageUploadService;
+import com.sohan.DocUploader.service.MultiUploadService;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+@Service
+public class UploadController {
+    private ImageUploadService imageUploadService;
+    private MultiUploadService multiUploadService;
+    private SingleTextUploadService singleTextUploadService;
+
+    public UploadController(ImageUploadService imageUploadService,
+                            MultiUploadService multiUploadService,
+                            SingleTextUploadService singleTextUploadService) throws IOException {
+        this.imageUploadService = imageUploadService;
+        this.multiUploadService = multiUploadService;
+        this.singleTextUploadService = singleTextUploadService;
+        getMultiUpload();
+    }
+
+    public void getSingleText() throws IOException {
+        singleTextUploadService.call();
+    }
+    public void getSingleUpload() throws IOException {
+        String path="D:/SprngScurty/IMAGES/upload/";
+        String file=path+"image02.jpg";
+        imageUploadService.upload(path,file);
+    }
+    public void getMultiUpload() throws IOException {
+        String url="http://localhost:8080/upload";
+        String path="D:/SprngScurty/IMAGES/random/";
+        multiUploadService.uploadAllFiles(path,url);
+    }
+
+}
