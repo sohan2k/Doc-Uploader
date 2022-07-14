391cfc11426821b8297a004a99f80cd8d3bfe26c
Sohan Barman <barmansohan9@mail.com>
2022-07-14 23:16:21 +0530

    single image upload service

--- src/main/java/com/sohan/DocUploader/service/ImageUploadService.java
+++ src/main/java/com/sohan/DocUploader/service/ImageUploadService.java
@@ -11,30 +11,16 @@ import java.util.List;
 @Service
 public class ImageUploadService {
 
-    public String path="D:/SprngScurty/IMAGES/upload/";//path="D:\\SprngScurty\\IMAGES";
+    //public String path="D:/SprngScurty/IMAGES/upload/";//path="D:\\SprngScurty\\IMAGES";
 
     OkHttpClient client = new OkHttpClient();
     MediaType mediaType = MediaType.parse("multipart/form-data; " );
-    List<String> docList=new ArrayList<>();
-    public List<String> getAllFiles(){
-        File folder = new File(path);
 
-        File[] listOfFiles = folder.listFiles();
-        for(int i=0;i<listOfFiles.length;i++){
-            docList.add(listOfFiles[i].getName());
-        }
-        System.out.println(docList);
-        return docList;
-    }
-
-    public void upload() throws IOException {
-//        RequestBody body = RequestBody.create(mediaType, "------WebKitFormBoundary7MA4YWxkTrZu0gW\r" +
-//                "\nContent-Disposition: form-data; name=\"file\"; filename=\"image02.jpg\"" +
-//                "\r\nContent-Type: image/jpeg\r\n\r\n\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--");
+    public void upload(String filename,String uploadedFile) throws IOException {
         RequestBody fbody=new MultipartBody.Builder()
                 .setType(MultipartBody.FORM)
-                .addFormDataPart("file",getAllFiles().get(3),RequestBody.create(
-                        mediaType,new File(path+getAllFiles().get(3))
+                .addFormDataPart("file",filename,RequestBody.create(
+                        mediaType,new File(uploadedFile)
                 )).build();
         Request request = new Request.Builder()
                 .url("http://localhost:8080/upload")
@@ -44,11 +30,11 @@ public class ImageUploadService {
                 .addHeader("postman-token", "96700d83-a4c0-f30d-4826-791278ad679c")
                 .build();
 
-        Response response = client.newCall(request).execute();
+        client.newCall(request).execute();
     }
 
-    public ImageUploadService() throws IOException {
-        upload();
-        getAllFiles();
-    }
+//    public ImageUploadService() throws IOException {
+//        upload();
+//        getAllFiles();
+//    }
 }
